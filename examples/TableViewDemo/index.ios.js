e92187cd414ba37651de316d7eb1c35d1370de4a
nyura123 <nyura123@gmail.com>
2016-02-07 19:28:07 -0600

    rounded radius in a few demo examples

--- examples/TableViewDemo/index.ios.js
+++ examples/TableViewDemo/index.ios.js
@@ -359,11 +359,13 @@ class TableViewExample extends React.Component {
 //Should be pure... setState on top-level component doesn't seem to work
 class TableViewExampleCell extends React.Component {
     render(){
-        var style = {};
+        var style = {borderColor:"#aaaaaa", borderWidth:1, borderRadius:3};
         //cell height is passed from <Item> child of tableview and native code passes it back up to javascript in "app params" for the cell.
         //This way our component will fill the full native table cell height.
         if (this.props.data.height !== undefined) {
             style.height = this.props.data.height;
+        } else {
+            style.flex = 1;
         }
         if (this.props.data.backgroundColor !== undefined) {
             style.backgroundColor = this.props.data.backgroundColor;
